8515486060fbf827ab15c4b77ee4101dcb2a3396
Griffith <arnaultcaquineau@gmail.com>
2017-05-10 11:18:25 +0200

    mise en place de l'api facebook

---
+++ application/views/apifacebook/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+    <body>
+        <link href="<?= base_url ("../node_modules/bootstrap/dist/css/bootstrap.min.css") ?>" rel="stylesheet">
+    
+    <!-- Latest compiled and minified CSS & JS -->
+    <link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    
+        <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+    <div class="row">
+
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam nisi vero reiciendis nulla doloribus doloremque voluptates libero saepe officiis, enim rerum quasi magnam ipsam dignissimos sapiente nam atque eius eveniet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur ut, voluptatum, et voluptatem in cum, ducimus fugit quasi expedita vitae consequuntur, obcaecati officia inventore explicabo accusantium. Deleniti possimus, unde consectetur.</p>
+        </div>
+
+        <div style="width:300px; height: 300px; overflow:scroll;" class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+            <div class="fb-post" data-href="https://www.facebook.com/20531316728/posts/10154009990506729/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/20531316728/posts/10154009990506729/" class="fb-xfbml-parse-ignore">Posted by <a href="https://www.facebook.com/facebook/">Facebook</a> on&nbsp;<a href="https://www.facebook.com/20531316728/posts/10154009990506729/">Thursday, August 27, 2015</a></blockquote></div>
+            <br>
+            <div class="fb-post" data-href="https://www.facebook.com/francoise.schepmans.1/posts/1737709209854543" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/francoise.schepmans.1/posts/1737709209854543" class="fb-xfbml-parse-ignore"><p>Votre r&#xf4;le est tr&#xe8;s difficile. ce n&#039;est pas le cas de beaucoup d&#039;autre &#xe9;lu MR. FELICITATIONS</p>Posted by <a href="#" role="button">Georges Mathonet</a> on&nbsp;<a href="https://www.facebook.com/francoise.schepmans.1/posts/1737709209854543">Tuesday, April 25, 2017</a></blockquote></div>
+            <br>
+            <div class="fb-post" data-href="https://www.facebook.com/francoise.schepmans.1/posts/1732412223717575" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/francoise.schepmans.1/posts/1732412223717575" class="fb-xfbml-parse-ignore"><p>J&#039;ai re&#xe7;u la visite de Hans van Baalen, d&#xe9;put&#xe9; europ&#xe9;en et pr&#xe9;sident du parti europ&#xe9;en de l&#039;Alliance des lib&#xe9;raux et des...</p>Posted by <a href="https://www.facebook.com/francoise.schepmans.1">Françoise Schepmans</a> on&nbsp;<a href="https://www.facebook.com/francoise.schepmans.1/posts/1732412223717575">Thursday, April 13, 2017</a></blockquote></div>
+        </div>
+
+    </div>
+    
+    </body>
+</html>
+
+<?php 
