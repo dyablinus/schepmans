0553d09de4c4ad80a666ea41e8aec477d2f969b1
Damian <damie236@outlook.com>
2017-05-11 13:47:40 +0200

    finish contact page static

--- application/views/template/base/nav.php
+++ application/views/template/base/nav.php
@@ -1,3 +1,7 @@
+<div class="container">
+
+<header>
+
 
 <nav class="navbar navbar-default" id="background-blank" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -71,7 +75,9 @@
     </div><!-- /.navbar-collapse -->
 </nav>
 
+</header>
 
+</div>
 
 
 
